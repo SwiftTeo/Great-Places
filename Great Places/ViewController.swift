a03eca44ad9d652efdf819890abdb4b5e39ad913
Teoman Dogueri <teo_dogu@yahoo.de>
2018-04-26 09:46:01 +0200

    Initial Commit

---
+++ Great Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Great Places
+//
+//  Created by AppleEnthusiast on 26.04.18.
+//  Copyright © 2018 TheProgrammingJedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
